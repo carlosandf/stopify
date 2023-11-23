e36ddaf607ccc9e031a3612e48e70df7ba5dc1b2
carlosandf <carlos.andres.ff2912@gmail.com>
2023-11-22 19:30:54 -0500

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: '#121212',
+				white: {
+					1: '#e9e0e0',
+					2: '#a7a7a7'
+				}
+			}
+		},
 	},
 	plugins: [],
 }
